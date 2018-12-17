7e18d549d6d803c71a1b8a89c8714107152e80f1
wangdou <742985389@qq.com>
2018-12-17 16:08:20 +0800

    分支提交模块

--- testdemo/src/main/java/com/jk/TestdemoApplication.java
+++ testdemo/src/main/java/com/jk/TestdemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestdemoApplication {
 
     public static void main(String[] args) {
+        System.out.print("提交分支内容");
         SpringApplication.run(TestdemoApplication.class, args);
     }
 
